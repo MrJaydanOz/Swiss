2185850e8297e702ade5d2c4326fc82f0acff2b8
MrJaydanOz <jaydan@jaysanlunt.com.au>
2025-04-02 16:23:54 +1100

    Modified IsEmpty.cs to include HasMoreThan(), HasLessThan(), HasExactly(), etc

--- Func/IsEmpty.cs
+++ Func/IsEmpty.cs
@@ -9,7 +9,9 @@ namespace Swiss
     {
         public static bool TryGetCount(this IEnumerable collection, out int count)
         {
-            if (collection is IReadOnlyCollection<object> collectionAsReadonlyCollection)
+            if (collection == null)
+                throw Expt.Null(nameof(collection));
+            else if (collection is IReadOnlyCollection<object> collectionAsReadonlyCollection)
             {
                 count = collectionAsReadonlyCollection.Count;
                 return true;
@@ -53,6 +55,8 @@ namespace Swiss
         public static bool IsNullOrEmpty<T>(this IReadOnlyCollection<T> collection) => collection == null || collection.Count == 0;
         public static bool IsNullOrEmpty<T>(this ICollection<T> collection) => collection == null || collection.Count == 0;
         public static bool IsNullOrEmpty<T>(this T[] collection) => collection == null || collection.Length == 0;
+        public static bool IsNullOrEmpty<T>(this Span<T> collection) => collection == null || collection.Length == 0;
+        public static bool IsNullOrEmpty<T>(this ReadOnlySpan<T> collection) => collection == null || collection.Length == 0;
 
         public static bool IsNullOrHasLessThan(this IEnumerable collection, int count)
         {
@@ -86,12 +90,48 @@ namespace Swiss
         public static bool IsNullOrHasLessThan<T>(this IReadOnlyCollection<T> collection, int count) => collection == null || collection.Count < count;
         public static bool IsNullOrHasLessThan<T>(this ICollection<T> collection, int count) => collection == null || collection.Count < count;
         public static bool IsNullOrHasLessThan<T>(this T[] collection, int count) => collection == null || collection.Length < count;
+        public static bool IsNullOrHasLessThan<T>(this Span<T> collection, int count) => collection == null || collection.Length < count;
+        public static bool IsNullOrHasLessThan<T>(this ReadOnlySpan<T> collection, int count) => collection == null || collection.Length < count;
         
+        public static bool IsNullOrHasNotExactly(this IEnumerable collection, int count)
+        {
+            if (collection == null)
+                return true;
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            return count != 0;
+        }
+        public static bool IsNullOrHasNotExactly<T>(this IEnumerable<T> collection, int count)
+        {
+            if (collection == null)
+                return true;
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            enumerator.Dispose();
+            return count != 0;
+        }
+        public static bool IsNullOrHasNotExactly<T>(this IReadOnlyCollection<T> collection, int count) => collection == null || collection.Count != count;
+        public static bool IsNullOrHasNotExactly<T>(this ICollection<T> collection, int count) => collection == null || collection.Count != count;
+        public static bool IsNullOrHasNotExactly<T>(this T[] collection, int count) => collection == null || collection.Length != count;
+        public static bool IsNullOrHasNotExactly<T>(this Span<T> collection, int count) => collection == null || collection.Length != count;
+        public static bool IsNullOrHasNotExactly<T>(this ReadOnlySpan<T> collection, int count) => collection == null || collection.Length != count;
 
         public static bool IsEmpty(this IEnumerable collection)
         {
             if (collection == null)
-                throw new NullReferenceException();
+                throw Expt.Null(nameof(collection));
 
             if (collection.TryGetCount(out var collectionLength))
                 return collectionLength == 0;
@@ -102,7 +142,7 @@ namespace Swiss
         public static bool IsEmpty<T>(this IEnumerable<T> collection)
         {
             if (collection == null)
-                throw new NullReferenceException();
+                throw Expt.Null(nameof(collection));
 
             if (collection.TryGetCount(out var collectionLength))
                 return collectionLength == 0;
@@ -112,14 +152,16 @@ namespace Swiss
             enumerator.Dispose();
             return result;
         }
-        public static bool IsEmpty<T>(this IReadOnlyCollection<T> collection) => collection == null ? throw new NullReferenceException() : collection.Count == 0;
-        public static bool IsEmpty<T>(this ICollection<T> collection) => collection == null ? throw new NullReferenceException() : collection.Count == 0;
-        public static bool IsEmpty<T>(this T[] collection) => collection == null ? throw new NullReferenceException() : collection.Length == 0;
+        public static bool IsEmpty<T>(this IReadOnlyCollection<T> collection) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count == 0;
+        public static bool IsEmpty<T>(this ICollection<T> collection) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count == 0;
+        public static bool IsEmpty<T>(this T[] collection) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == 0;
+        public static bool IsEmpty<T>(this Span<T> collection) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == 0;
+        public static bool IsEmpty<T>(this ReadOnlySpan<T> collection) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == 0;
 
         public static bool HasLessThan(this IEnumerable collection, int count)
         {
             if (collection == null)
-                throw new NullReferenceException();
+                throw Expt.Null(nameof(collection));
 
             if (collection.TryGetCount(out var collectionLength))
                 return collectionLength < count;
@@ -133,7 +175,7 @@ namespace Swiss
         public static bool HasLessThan<T>(this IEnumerable<T> collection, int count)
         {
             if (collection == null)
-                throw new NullReferenceException();
+                throw Expt.Null(nameof(collection));
 
             if (collection.TryGetCount(out var collectionLength))
                 return collectionLength < count;
@@ -145,8 +187,80 @@ namespace Swiss
             enumerator.Dispose();
             return count > 0;
         }
-        public static bool HasLessThan<T>(this IReadOnlyCollection<T> collection, int count) => collection == null ? throw new NullReferenceException() : collection.Count < count;
-        public static bool HasLessThan<T>(this ICollection<T> collection, int count) => collection == null ? throw new NullReferenceException() : collection.Count < count;
-        public static bool HasLessThan<T>(this T[] collection, int count) => collection == null ? throw new NullReferenceException() : collection.Length < count;
+        public static bool HasLessThan<T>(this IReadOnlyCollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count < count;
+        public static bool HasLessThan<T>(this ICollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count < count;
+        public static bool HasLessThan<T>(this T[] collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length < count;
+        public static bool HasLessThan<T>(this Span<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length < count;
+        public static bool HasLessThan<T>(this ReadOnlySpan<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length < count;
+
+        public static bool HasMoreThan(this IEnumerable collection, int count)
+        {
+            if (collection == null)
+                throw Expt.Null(nameof(collection));
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            return count < 0;
+        }
+        public static bool HasMoreThan<T>(this IEnumerable<T> collection, int count)
+        {
+            if (collection == null)
+                throw Expt.Null(nameof(collection));
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            enumerator.Dispose();
+            return count < 0;
+        }
+        public static bool HasMoreThan<T>(this IReadOnlyCollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count > count;
+        public static bool HasMoreThan<T>(this ICollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count > count;
+        public static bool HasMoreThan<T>(this T[] collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length > count;
+        public static bool HasMoreThan<T>(this Span<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length > count;
+        public static bool HasMoreThan<T>(this ReadOnlySpan<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length > count;
+
+        public static bool HasExactly(this IEnumerable collection, int count)
+        {
+            if (collection == null)
+                throw Expt.Null(nameof(collection));
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            return count == 0;
+        }
+        public static bool HasExactly<T>(this IEnumerable<T> collection, int count)
+        {
+            if (collection == null)
+                throw Expt.Null(nameof(collection));
+
+            if (collection.TryGetCount(out var collectionLength))
+                return collectionLength < count;
+
+            var enumerator = collection.GetEnumerator();
+            while (count >= 0 && enumerator.MoveNext())
+                count--;
+
+            enumerator.Dispose();
+            return count == 0;
+        }
+        public static bool HasExactly<T>(this IReadOnlyCollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count == count;
+        public static bool HasExactly<T>(this ICollection<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Count == count;
+        public static bool HasExactly<T>(this T[] collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == count;
+        public static bool HasExactly<T>(this Span<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == count;
+        public static bool HasExactly<T>(this ReadOnlySpan<T> collection, int count) => collection == null ? throw Expt.Null(nameof(collection)) : collection.Length == count;
     }
 }
